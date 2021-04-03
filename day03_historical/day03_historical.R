320329bb5c84789d62c50baae91dad09dfc80b20
richardvogg <46679869+richardvogg@users.noreply.github.com>
2021-04-03 22:49:13 +0200

    cleaned code

--- day03_historical/day03_historical.R
+++ day03_historical/day03_historical.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(readxl)
 library(ggplot2)
 library(ggradar)
+library(patchwork)
 
 sysfonts::font_add_google(name = "Prata", "Prata")
 showtext::showtext_auto()
@@ -14,18 +15,24 @@ df <- lapply(2006:2020,function(x) {
   do.call(rbind,.) %>%
   mutate(Year=format(Year,"%Y"))
 
+# calculate global mean values
 df_long <- df %>%
   filter(Year %in% c(2010,2020)) %>%
   group_by(Year) %>%
   summarise(across(4:15,.fns = function(x) mean(x)/10))
 
+#clean names
 colnames(df_long)[-1] <- colnames(df_long)[-1] %>%
   substr(5,100) %>%
-  stringr::str_wrap(15)
+  stringr::str_wrap(12)
 
-average <- df_long %>% ggradar(font.radar = "Circular Air",
-                    values.radar = c("1 (high\nstability)","", "10"),
-                    legend.position = "top")
+#global mean radar
+average <- df_long %>% ggradar(font.radar = "Prata",
+                               base.size=8,
+                               grid.label.size=5,
+                               axis.label.size=3,
+                    values.radar = c("1","", "10"),
+                    legend.position = "bottom")
 
 # Top / Flop 3
 
@@ -54,32 +61,38 @@ out <- lapply(top_flop_3, function(x) {
   df_long %>% 
   filter(Country==x) %>%
   select(-Country) %>% 
-  ggradar(font.radar = "Circular Air",
+  ggradar(font.radar = "Prata",
           axis.labels = rep("",12),
           values.radar = c("","", ""),
           legend.position = "none",
+          group.point.size = 3,
+          group.line.width = 1,
           plot.title = x)
 })
 
-library(patchwork)
 
-average +
-((out[[1]]+out[[2]]+out[[3]]) /
-  (out[[4]] + out[[5]] + out[[6]])) +
-  plot_annotation(
-    
-  )
+## Assemble the plot
+
+(average | (out[[1]]+out[[2]]+out[[3]] + 
+   out[[4]] + out[[5]] + out[[6]])) +
+  plot_annotation(title = "The Fragile State Index",
+                  subtitle = paste("The index measures vulnerability of countries based on 12 indicators which take values between 1 and 10",
+                                   "where 1 is high stability and 10 is high vulnerability. The global mean is shown on the left, and we see that on",
+                                   "average most indicators improved between 2010 and 2020. On the left we see the three countries which improved most",
+                                   "(Cuba, Gerogia and Moldova) and the three countries which worsened most (Mali, Syria, Libya).",sep = "\n")) &
+  theme(plot.title = element_text(size = 20, family = "Prata"),
+        plot.subtitle = element_text(size = 15, family = "Prata"))
 
 
-ggplot()+
-  geom_text(aes(x=0,y=0,label=paste("The index measures vulnerability of countries",
-                  "based on 12 indicators which take values between 1 and 10",
-                  "where 1 is high stability and 10 is high vulnerability.",
-                  "We see the global average on the left, and see that on",
-                  "average most indicators improved between 2010 and 2020.",
-                  "On the left we see the three countries which improved most",
-                  "(Cuba, Gerogia and Moldova) and the three countries which",
-                  "worsened most (Mali, Syria, Libya).",sep = "\n")),
-            hjust = 0, vjust = 1)+
-  expand_limits(x=c(0,1),y=c(0,1))+
-  labs(title = "The Fragile State Index")
+## alternative with layout
+
+
+layout <- "
+AAAABBCCDD
+AAAABBCCDD
+AAAAEEFFGG
+AAAAEEFFGG
+"
+
+average + out[[1]] + out[[2]] + out[[3]] +
+  out[[4]] + out[[5]] +out[[6]] +plot_layout(design = layout)
