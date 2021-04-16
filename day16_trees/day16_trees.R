898e53877d117f0beaf800c52f22ef2dc743da1a
richardvogg <46679869+richardvogg@users.noreply.github.com>
2021-04-16 09:50:32 +0200

    added larger tree

--- day16_trees/day16_trees.R
+++ day16_trees/day16_trees.R
@@ -12,6 +12,27 @@ tree <- rpart(factor(Outcome)~., data=df, method = "class",
               control = ctrl)
 
 
+rpart.plot(tree,
+           box.palette = c("orangered2","grey","cyan2"),
+           branch.col = c("goldenrod"),
+           branch.lwd = 3,
+           tweak = 1.2,
+           varlen = 15,
+           branch = 0.1,
+           clip.right.labs = FALSE,
+           type = 3,
+           cex.main = 1.5,
+           leaf.round = 9,
+           fallen.leaves = TRUE,
+           main = "Factors for diabetes (dataset with 768 females with Pima Indian heritage from Kaggle)")
+
+
+# larger tree
+
+ctrl <- list(cp = 0.01, minbucket = 10, maxdepth = 5)
+
+tree <- rpart(factor(Outcome)~., data=df, method = "class",
+              control = ctrl)
 
 
 rpart.plot(tree,
