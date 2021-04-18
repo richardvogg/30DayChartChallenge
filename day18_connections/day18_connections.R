c2ed787accd381b64ef535fb50f149bceebf6071
richardvogg <46679869+richardvogg@users.noreply.github.com>
2021-04-18 09:18:32 +0200

    Update day18_connections.R

--- day18_connections/day18_connections.R
+++ day18_connections/day18_connections.R
@@ -1,14 +1,17 @@
 library(dplyr)
 library(ggplot2)
-library(tidytext)
+library(tidytext) # unnest the tags columns
 library(reticulate) #Python connection
-library(forcats)
-library(widyr)
-library(igraph)
-library(ggraph)
+library(widyr) #prepare the data for the network
+library(igraph) # network
+library(ggraph) # network
+library(ggfx) # for outer glow
 
 use_python("C:/Users/Richard/anaconda3",required=TRUE)
 source_python("day17_pop_culture/change_encoding.py")
+
+sysfonts::font_add_google(name = "Fredoka One", "Fredoka")
+showtext::showtext_auto()
 
 
 options(scipen = 10)
@@ -51,7 +54,7 @@ all_tags <- youtube %>%
   pairwise_similarity(title, tags, value, upper = FALSE)
   #pairwise_dist(title,tags,value,upper=FALSE) 
 
-set.seed(63)
+
 
 net <- all_tags %>%
   graph_from_data_frame()
@@ -62,24 +65,32 @@ test <- data.frame(name=names(V(net))) %>%
 V(net)$views <- test$views
 V(net)$category <- test$category
 
-library(ggfx)
+set.seed(64)
 
 net %>%
   ggraph(layout="fr") +
   with_outer_glow(
-    geom_edge_link(aes(edge_alpha = similarity, width = similarity)),
+    geom_edge_link(aes(edge_alpha = similarity)),
     sigma = 0.1
   ) +
   with_outer_glow(
-    geom_node_point(aes(col=factor(category), size = views))
+    geom_node_point(aes(col=factor(category), size = views)),
+    id = "nodes", col = "white"
   ) +
+  with_inner_glow("nodes", sigma = 0.1)+
   geom_node_text(aes(label=name), size=3, repel=TRUE)+
   scale_edge_alpha(range=c(0.5,0.1))+
   scale_edge_width(range=c(0.1,3)) +
-  guides(size = FALSE, edge_alpha = FALSE, edge_width = FALSE) +
+  scale_color_brewer(palette = "Dark2") +
+  guides(size = FALSE, edge_alpha = FALSE) +
   labs(title = "Top Youtube Trending Videos in the US 2017/18",
-       subtitle = paste("The more common tags two videos share, the stronger the connection.",
+       subtitle = paste("If two videos share a common tag on Youtube, they have a connection.",
                         "\nSize of the circle shows the number of views."),
        colour = "Category") +
-  theme_void()
+  theme_void() +
+  theme(panel.background = element_rect(fill = "grey80"),
+        plot.background = element_rect(fill = "grey80"),
+        plot.title = element_text(size = 20, family = "Fredoka"),
+        plot.subtitle = element_text(size = 16),
+        legend.position = c(0.1,0.15))
 
